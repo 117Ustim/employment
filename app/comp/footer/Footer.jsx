74ebf041de8964715ece8bc73e0f00fa420cb03e
117Ustim <ustik72@gmail.com>
2025-06-15 21:20:11 +0300

    15/06

--- app/comp/footer/Footer.jsx
+++ app/comp/footer/Footer.jsx
@@ -1,11 +1,27 @@
 
-import styles from'./footer.module.scss';
+import styles from './footer.module.scss';
+import { FaTelegramPlane, FaViber, FaWhatsapp, FaVk } from 'react-icons/fa';
 
-
-export default function Footer () {
-return (
- <div className={styles.container}>
-Footer
-</div>
-);
+export default function Footer() {
+  return (
+    <div className={styles.container}>
+      <p className={styles.text}>
+        Если у вас остались вопросы, пожалуйста, свяжитесь с нами через любой удобный для вас мессенджер или напишите нам на почту: <a href="mailto:gironarecruitmentsl@outlook.com">gironarecruitmentsl@outlook.com</a>
+      </p>
+      <div className={styles.icons}>
+        <a href="https://t.me/" target="_blank" rel="noopener noreferrer">
+          <FaTelegramPlane />
+        </a>
+        <a href="viber://chat" target="_blank" rel="noopener noreferrer">
+          <FaViber />
+        </a>
+        <a href="https://wa.me/" target="_blank" rel="noopener noreferrer">
+          <FaWhatsapp />
+        </a>
+        <a href="https://vk.com/" target="_blank" rel="noopener noreferrer">
+          <FaVk />
+        </a>
+      </div>
+    </div>
+  );
 }
