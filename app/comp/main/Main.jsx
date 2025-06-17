6ba78b6273954fd45326967a3c1fc9b09b0e1611
117Ustim <ustik72@gmail.com>
2025-06-17 15:35:54 +0300

    17.06.2

--- app/comp/main/Main.jsx
+++ app/comp/main/Main.jsx
@@ -2,15 +2,15 @@
 
 import { motion } from "framer-motion";
 import styles from './main.module.scss';
-import {main_text } from "../../../data";
+import { main_text } from "../../../data";
 
 export default function Main() {
   return (
     <div className={styles.container}>
-   
+
       <video
         className={styles.video}
-        src="/video_main_1.mp4" 
+        src="/video_main_1.mp4"
         autoPlay
         loop
         muted
@@ -23,17 +23,36 @@ export default function Main() {
      
       <motion.div
         className={styles.content}
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
       >
-        <h1 className={styles.title}>
+        <motion.h1
+          className={styles.title}
+          initial={{ x: -150, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
           {main_text[0].title}
-        </h1>
-        <p className={styles.text}>
+        </motion.h1>
+
+        <motion.p
+          className={styles.text}
+          initial={{ x: 150, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 0.4 }}
+        >
           {main_text[0].text}
-        </p>
-        <button className={styles.button}>Связаться с нами</button>
+        </motion.p>
+
+        <motion.button
+          className={styles.button}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 0.8 }}
+        >
+          Связаться с нами
+        </motion.button>
       </motion.div>
     </div>
   );
