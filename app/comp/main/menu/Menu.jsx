244d8b0326f2036c2da74de11ebe45b5ab6e5fb1
117Ustim <ustik72@gmail.com>
2025-06-17 15:41:43 +0300

    17.06.2

--- app/comp/main/menu/Menu.jsx
+++ app/comp/main/menu/Menu.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { motion } from "framer-motion";
 import Burger from "../burger/Burger";
 import styles from "./menu.module.scss";
 
@@ -11,32 +12,98 @@ export default function Menu() {
     }
   };
 
+
+  const containerVariants = {
+    hidden: {},
+    visible: {
+      transition: {
+        staggerChildren: 0.5, // Задержка между кнопками
+      },
+    },
+  };
+
+
+  const buttonVariants = {
+    hidden: { opacity: 0, y: -20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+  };
+
   return (
     <>
-    <nav className={styles.menu}>
-      <div className={styles.left}>
-        <button  className={styles.menuButton}onClick={() => scrollToSection("main")}>Главная</button>
-        <button  className={styles.menuButton}onClick={() => scrollToSection("history")}>О нас</button>
-         <button  className={styles.menuButton}onClick={() => scrollToSection("services")}>Услуги</button>
-      </div>
+      <nav className={styles.menu}>
+        <motion.div
+          className={styles.left}
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.button
+            className={styles.menuButton}
+            variants={buttonVariants}
+            onClick={() => scrollToSection("main")}
+          >
+            Главная
+          </motion.button>
+          <motion.button
+            className={styles.menuButton}
+            variants={buttonVariants}
+            onClick={() => scrollToSection("history")}
+          >
+            О нас
+          </motion.button>
+          <motion.button
+            className={styles.menuButton}
+            variants={buttonVariants}
+            onClick={() => scrollToSection("services")}
+          >
+            Услуги
+          </motion.button>
+        </motion.div>
 
-      <div className={styles.logo}>
-         <div className={styles.logo_block} onClick={() => scrollToSection("main")} style={{ cursor: 'pointer' }}>
-       <img src="/logo.png" alt="Logo" className={styles.logoImage} />
-      <img src="/logo_text.png" alt="LogoText" className={styles.logoText} /> 
-        </div>
-    
-      </div>
-
-      <div className={styles.right}>
-        <button className={styles.menuButton}onClick={() => scrollToSection("vacancies")}>Вакансии</button>
-         <button className={styles.menuButton}onClick={() => scrollToSection("team")}>Контакты</button>
-        <button className={styles.menuButton}onClick={() => scrollToSection("reviews")}>Отзывы</button>
-      </div>
-    </nav>
-    <div className={styles.menu_1000}>
-            <Burger/>
+        <div className={styles.logo}>
+          <div
+            className={styles.logo_block}
+            onClick={() => scrollToSection("main")}
+            style={{ cursor: "pointer" }}
+          >
+            <img src="/logo.png" alt="Logo" className={styles.logoImage} />
+            <img src="/logo_text.png" alt="LogoText" className={styles.logoText} />
           </div>
-          </>
+        </div>
+
+        <motion.div
+          className={styles.right}
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.button
+            className={styles.menuButton}
+            variants={buttonVariants}
+            onClick={() => scrollToSection("vacancies")}
+          >
+            Вакансии
+          </motion.button>
+          <motion.button
+            className={styles.menuButton}
+            variants={buttonVariants}
+            onClick={() => scrollToSection("team")}
+          >
+            Контакты
+          </motion.button>
+          <motion.button
+            className={styles.menuButton}
+            variants={buttonVariants}
+            onClick={() => scrollToSection("reviews")}
+          >
+            Отзывы
+          </motion.button>
+        </motion.div>
+      </nav>
+
+      <div className={styles.menu_1000}>
+        <Burger />
+      </div>
+    </>
   );
 }
